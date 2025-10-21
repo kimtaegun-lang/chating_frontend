c400b9cd6bfb2c569766b4a554a987624819976b
김태근 <tee1694@naver.com>
2025-10-21 14:45:41 +0900

    accesstoken 및 refreshtoken 재발급 설정

--- src/api/rootApi.tsx
+++ src/api/rootApi.tsx
@@ -1,11 +1,12 @@
-import axios from 'axios';
+import axios, { AxiosError } from 'axios';
+
 const serverPort = "http://localhost:8080";
 
 const api = axios.create({
     baseURL: serverPort
 });
 
-// 모든 요청에 token 자동 추가
+// http 요청 인터셉터 설정 api를 사용해서 요청할 때마다 토큰을 헤더에 포함
 api.interceptors.request.use((config) => {
     const token = localStorage.getItem('accessToken');
     if (token) {
@@ -13,5 +14,47 @@ api.interceptors.request.use((config) => {
     }
     return config;
 });
+
+// http 응답 인터셉터 설정  토큰 만료 시 재발급 로직
+api.interceptors.response.use(
+    response => response,
+    async (error: AxiosError) => {
+        const originalRequest = error.config as any;
+        
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true;
+            
+            const refreshToken = localStorage.getItem('refreshToken');
+            
+            if (!refreshToken) {
+                console.log("Refresh Token 없음");
+                localStorage.clear();
+                window.location.href = '/login';
+                return Promise.reject(error);
+            }
+
+            try {
+                const result = await axios.post(`${serverPort}/api/refresh`, { refreshToken });
+                
+                if (result.data.accessToken) {
+                    localStorage.setItem('accessToken', result.data.accessToken);
+                    originalRequest.headers.Authorization = `Bearer ${result.data.accessToken}`;
+                    console.log("토큰 재발급 완료");
+                    return axios(originalRequest);
+                }
+            } catch (refreshError) {
+                if (axios.isAxiosError(refreshError) && refreshError.response?.status === 403) {
+                    console.error("Refresh Token 만료");
+                    alert('세션이 만료되었습니다. 다시 로그인해주세요.');
+                    localStorage.clear();
+                    window.location.href = '../member/signIn';
+                }
+                return Promise.reject(refreshError);
+            }
+        }
+        
+        return Promise.reject(error);
+    }
+);
 
 export { serverPort, api };
